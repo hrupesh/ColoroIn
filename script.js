589f65eb6f415de1b771ae5c251357b8fcd155f0
Satyam Kumar <33719556+Anonsatyam@users.noreply.github.com>
2020-07-17 14:12:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-var myColors = ["red", "blue", "green", "black"]
 
-for (var i = 0; i <= myColors.length; i++) {
-    console.log(myColors[i])
-    var childCards = document.getElementsByClassName('palette_body');
-    childCards.style.backgroundColor = myColors[i];
-}
